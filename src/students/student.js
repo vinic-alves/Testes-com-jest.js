e460ebda192ea86176a0138b238e6c58c2e57d91
vinic-alves <vinicius.alves95@outlook.com>
2022-09-22 21:07:59 -0300

    Testes com jest

---
+++ src/students/student.js
@@ -0,0 +1,19 @@
+// esse arquivo estará sendo exportado para area de testes
+
+module.exports = {
+    sum: notes => {
+
+        
+
+        if (Math.min(...notes)){
+            const result = notes.reduce((acumulador, nota) => {
+                acumulador += nota > 9 ? nota * 2 : nota
+                return acumulador
+            }, 0)
+            return notes.length > 5 ? result * 2 : result
+        }
+        return 0;
+
+        
+    }
+}
